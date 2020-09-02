4f0c1c1141dae495878cd254f9c2e5155c0b9ba8
babai1203 <souptik.chatterjee7@gmail.com>
2020-09-02 11:35:34 +0530

    new code base

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DataModelsComponent } from './data-models/data-models.component';
+import { TypesComponent } from './types/types.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    DataModelsComponent,
+    TypesComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
