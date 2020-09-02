d950509dbf09c7ee43cae33b790cc06d4ab2a59c
rnaaz <rohinanaaz@gmail.com>
2020-09-02 19:54:59 +0530

    added routes for entities, Added Css for entities,bug fix in header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DataModelDomainsComponent} from './data-model-domains/data-model-domains.component'
+import { EntitiesComponent } from './entities/entities.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'datamodels',
     component: DataModelDomainsComponent,
+  },
+  {
+    path: 'entities',
+    component: EntitiesComponent,
   },
 ];
 
