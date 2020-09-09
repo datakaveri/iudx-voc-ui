8d5729e75e8ac485f47c38496707b49c0481c572
Yash Patki <yash.patki1@gmail.com>
2020-09-09 03:08:02 -0700

    added routes for domains

--- src/app/schema-details-types/schema-details-types.component.ts
+++ src/app/schema-details-types/schema-details-types.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { InterceptorService } from '../interceptor.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-schema-details-types',
   templateUrl: './schema-details-types.component.html',
-  styleUrls: ['./schema-details-types.component.scss']
+  styleUrls: ['./schema-details-types.component.scss'],
 })
 export class SchemaDetailsTypesComponent implements OnInit {
   className: any;
+  value: any;
   parsed_response: any;
   breadcrumbs: Array<string>;
-  constructor( private route: ActivatedRoute,
-  private service :InterceptorService) {
-    this.parsed_response={};
+  constructor(
+    private route: ActivatedRoute,
+    private service: InterceptorService,
+    private router: Router
+  ) {
+    this.parsed_response = {};
     this.className = this.route.snapshot.params.class_name;
     this.breadcrumbs = [];
   }
@@ -21,25 +25,30 @@ export class SchemaDetailsTypesComponent implements OnInit {
   ngOnInit(): void {
     this.showClassDetail();
   }
-  showClassDetail(){
-    this.service.get_api_headersLD(this.className).then((data)=>{
+  showClassDetail() {
+    this.service.get_api_headersLD(this.className).then((data) => {
       let response = data['@graph'];
       this.parsed_response = {
         description: response[0]['rdfs:comment'],
-        breadcrumbs: [response[0]['@id'].split('iudx:')[1], response[0]['rdfs:subClassOf']['@id'].split('iudx:')[1]],
-        tables: {}
-      }
-      this.get_sub_class(response,response[0]['rdfs:subClassOf']['@id']);
+        breadcrumbs: [
+          response[0]['@id'].split('iudx:')[1],
+          response[0]['rdfs:subClassOf']['@id'].split('iudx:')[1],
+        ],
+        tables: {},
+      };
+      this.get_sub_class(response, response[0]['rdfs:subClassOf']['@id']);
       this.get_sub_tables(response);
       this.breadcrumbs = this.parsed_response.breadcrumbs.reverse();
     });
   }
   get_sub_class(arr, str) {
-    for(let i=0;i<arr.length;i++) {
-      if(arr[i]['@id'] == str) {
-        if(arr[i]['rdfs:subClassOf']) {
-          this.parsed_response.breadcrumbs.push(arr[i]['rdfs:subClassOf']['@id'].split('iudx:')[1]);
-          this.get_sub_class(arr,arr[i]['rdfs:subClassOf']['@id']);
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]['@id'] == str) {
+        if (arr[i]['rdfs:subClassOf']) {
+          this.parsed_response.breadcrumbs.push(
+            arr[i]['rdfs:subClassOf']['@id'].split('iudx:')[1]
+          );
+          this.get_sub_class(arr, arr[i]['rdfs:subClassOf']['@id']);
         }
         break;
       }
@@ -47,20 +56,27 @@ export class SchemaDetailsTypesComponent implements OnInit {
   }
 
   get_sub_tables(arr) {
-    this.parsed_response.breadcrumbs.forEach((b)=>{
+    this.parsed_response.breadcrumbs.forEach((b) => {
       this.parsed_response.tables[b] = [];
     });
-    for(let i=0;i<arr.length;i++) {
-      if(arr[i]['iudx:domainIncludes']) {
-        loop_inner:
-        for(let j=0;j<this.parsed_response.breadcrumbs.length;j++) {
-          if(this.is_includes(arr[i], this.parsed_response.breadcrumbs[j])) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]['iudx:domainIncludes']) {
+        loop_inner: for (
+          let j = 0;
+          j < this.parsed_response.breadcrumbs.length;
+          j++
+        ) {
+          if (this.is_includes(arr[i], this.parsed_response.breadcrumbs[j])) {
             let obj = {
               property: arr[i]['rdfs:label'],
-              expected_types: arr[i]['iudx:rangeIncludes'].map(x=>{ return x['@id'].split(':')[1] }),
-              description: arr[i]['rdfs:comment']
-            }
-            this.parsed_response.tables[this.parsed_response.breadcrumbs[j]].push(obj);
+              expected_types: arr[i]['iudx:rangeIncludes'].map((x) => {
+                return x['@id'].split(':')[1];
+              }),
+              description: arr[i]['rdfs:comment'],
+            };
+            this.parsed_response.tables[
+              this.parsed_response.breadcrumbs[j]
+            ].push(obj);
             break loop_inner;
           }
         }
@@ -70,10 +86,9 @@ export class SchemaDetailsTypesComponent implements OnInit {
 
   is_includes(obj, str) {
     let flag = false;
-    obj['iudx:domainIncludes'].forEach(b=>{
-        if(b['@id'] == ('iudx:' + str)) flag = true;
+    obj['iudx:domainIncludes'].forEach((b) => {
+      if (b['@id'] == 'iudx:' + str) flag = true;
     });
     return flag;
   }
-
 }
