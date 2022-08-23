3dcc6c93689f6bb5ce5741ae0fa22fe3cfdc1051
jvashishtha <jyoti.vashishtha@datakaveri.org>
2022-08-23 13:51:19 +0530

    REFACTOR: Have upgrade the angular version 11 from 10

--- src/app/data-model-domains/data-model-domains.component.spec.ts
+++ src/app/data-model-domains/data-model-domains.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DataModelDomainsComponent } from './data-model-domains.component';
 
@@ -6,7 +6,7 @@ describe('DataModelDomainsComponent', () => {
   let component: DataModelDomainsComponent;
   let fixture: ComponentFixture<DataModelDomainsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DataModelDomainsComponent ]
     })
